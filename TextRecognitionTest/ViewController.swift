477d4fc52962171f5c5c447d94f521eebc4f2b62
Michael Redig <mredig@gmail.com>
2019-10-31 15:25:56 -0500

    Initial Commit

---
+++ TextRecognitionTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextRecognitionTest
+//
+//  Created by Michael Redig on 10/31/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
