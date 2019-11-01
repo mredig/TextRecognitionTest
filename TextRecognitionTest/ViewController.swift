dcca0487e3a2cbfb60e2fce1b7a2c9e3063da948
Michael Redig <mredig@gmail.com>
2019-10-31 22:52:50 -0500

    rudimentary text recognition

--- TextRecognitionTest/ViewController.swift
+++ TextRecognitionTest/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		guard let image = UIImage(named: "test") else { return }
+		getText(from: image)
+
 	}
 
+	func getText(from image: UIImage) {
+		guard let cgImage = image.cgImage else { return }
+		let requestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+
+		let request = VNRecognizeTextRequest { request, error in
+			if let error = error {
+				print("Error recognizing text: \(error)")
+			}
+
+			guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
+			for currentObservation in observations {
+				let topCandidate = currentObservation.topCandidates(1)
+//				print("candidates: \(topCandidates.count)")
+//				for candidate in topCandidates {
+//				}
+				if let recognizedText = topCandidate.first {
+					print(recognizedText.string)
+				}
+			}
+		}
+		request.recognitionLevel = .accurate
+		request.revision = VNRecognizeTextRequestRevision1
+		request.usesLanguageCorrection = false
+//		request.reg
+
+		do {
+			try requestHandler.perform([request])
+		} catch {
+			NSLog("Error requesting text recognition: \(error)")
+		}
+	}
 
 }
-
